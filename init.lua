053ae4bdde575a4009bf503ebdda5751c8703823
Mahfuj A <mahfuj.ahbab@gmail.com>
2025-04-02 20:43:15 -0400

    added logic to bootstrap lazy.nvim on fresh install

--- init.lua
+++ init.lua
@@ -1,3 +1,14 @@
+-- Bootstrap Lazy.nvim if it isn't installed
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", lazypath
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
 -- init.lua
 vim.loader.enable()
 
